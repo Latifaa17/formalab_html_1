3e69c0b0b5acf150160aebc2c8b61405c3c35f5c
latifa <latifaadhibi1@gmail.com>
2019-03-26 12:44:17 +0100

    ecriture sur le console

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,9 @@
 alert ('hello world');
 var x=2;
-document.write("type de x:"+typeof x);
+console.log("type de x:"+typeof x);
 var h="5";
+document.write("</br>");
 document.write(h+1);
 h=Number("5");
+document.write("</br>");
 document.write(h+1);
